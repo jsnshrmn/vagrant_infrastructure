f072b31bda70a2d2924167b71d36d29cca75a6ab
Jason Sherman <jsn.sherman@gmail.com>
2016-10-21 05:43:20 -0500

    Really replace all vagrant.local references with vagrant.localdomain.

--- projects/cas/vagrant.rb
+++ projects/cas/vagrant.rb
@@ -2,7 +2,7 @@
 
 # CAS
 config.vm.define "cas" do |cas|
-  cas.vm.hostname = "cas.vagrant.local"
+  cas.vm.hostname = "cas.vagrant.localdomain"
   cas.vm.network "forwarded_port", guest:8443, host:8443
   cas.vm.provider :virtualbox do |v|
     v.memory = 1024
@@ -11,11 +11,11 @@ end
 
 # Drupal 7
 config.vm.define "d7" do |d7|
-  d7.vm.hostname = "d7.vagrant.local"
+  d7.vm.hostname = "d7.vagrant.localdomain"
 end
 
 # Nginx
 config.vm.define "nginx" do |nginx|
-  nginx.vm.hostname = "nginx.vagrant.local"
+  nginx.vm.hostname = "nginx.vagrant.localdomain"
   nginx.vm.network "forwarded_port", guest:443, host:64443
 end
