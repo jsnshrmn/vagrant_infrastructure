78858fe0b9596ecadda749fba0cb7ee9a4a91fc7
Logan Cox <lmc@ou.edu>
2017-03-02 17:22:16 -0600

    Increase resources for Islandora box.

--- projects/islandora/vagrant.rb
+++ projects/islandora/vagrant.rb
@@ -2,6 +2,12 @@
 
 # Drupal 7
 config.vm.define( "islandora") do |d7|
+
+  d7.vm.provider :virtualbox do |v|
+    v.memory = 3000  # Keeping overhead low
+    v.cpus = 3
+  end
+
   d7.vm.hostname = "islandora.vagrant.localdomain"
   d7.vm.network "private_network", ip: "192.168.96.8", :netmask => "255.255.255.0"
 end
