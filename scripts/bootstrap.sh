797172afb993504b5445e5642809bfb7db952e6b
Jason Sherman <jsn.sherman@gmail.com>
2017-03-16 11:08:49 -0500

    Remove versionlock from sbootstrap script since it's now a pretask for all hosts.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -18,10 +18,6 @@ done
 yum clean metadata
 yum check-update
 
-# Lock kernel version
-yum install -y yum-plugin-versionlock
-yum versionlock kernel
-
 # Install git and ansible to get started
 yum install -y git
 yum install -y python-pip
