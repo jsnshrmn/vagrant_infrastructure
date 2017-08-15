556a43e5546ac629994440bff2c21c112e5036ed
Logan Cox <lmc@ou.edu>
2017-08-15 01:00:04 -0500

    Give DSpace more RAM.

--- projects/dspace/vagrant.rb
+++ projects/dspace/vagrant.rb
@@ -4,4 +4,10 @@
 config.vm.define "dspace" do |dspace|
   dspace.vm.hostname = "dspace.vagrant.localdomain"
   dspace.vm.network "private_network", ip: "192.168.96.10", :netmask => "255.255.255.0"
+
+  # DSpace is a RAM Hungry Java App
+  dspace.vm.provider :virtualbox do |v|
+    v.memory = 1024  
+  end
+  
 end
