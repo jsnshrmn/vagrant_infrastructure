bbb7d0df63583921751683e629713ec44379d60d
Logan Cox <lmc@ou.edu>
2017-10-31 14:28:14 -0500

    More ram and cpu for the d7 box.

--- projects/web/vagrant.rb
+++ projects/web/vagrant.rb
@@ -10,6 +10,10 @@ end
 config.vm.define "d7" do |d7|
   d7.vm.hostname = "d7.vagrant.localdomain"
   d7.vm.network "private_network", ip: "192.168.96.4", :netmask => "255.255.255.0"
+  d7.vm.provider :virtualbox do |v|
+    v.memory = 2000 
+    v.cpus = 2 
+  end
 end
 
 # CAS
