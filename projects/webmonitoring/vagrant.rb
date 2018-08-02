a0e9fa19fd7b40e11b9fd9493647e05d23cee9b2
Jason Sherman <jsn.sherman@gmail.com>
2018-08-02 09:51:12 -0500

    define machines properly.

--- projects/webmonitoring/vagrant.rb
+++ projects/webmonitoring/vagrant.rb
@@ -1,16 +1,16 @@
 # See main Vagrantfile for default settings and provisioners
 
 config.vm.define "diff" do |diff|
-  dev.vm.hostname = "diff.vagrant.localdomain"
-  dev.vm.network "private_network", ip: "192.168.96.12", :netmask => "255.255.255.0"
+  diff.vm.hostname = "diff.vagrant.localdomain"
+  diff.vm.network "private_network", ip: "192.168.96.12", :netmask => "255.255.255.0"
 end
 
 config.vm.define "api" do |api|
-  dev.vm.hostname = "api.vagrant.localdomain"
-  dev.vm.network "private_network", ip: "192.168.96.13", :netmask => "255.255.255.0"
+  api.vm.hostname = "api.vagrant.localdomain"
+  api.vm.network "private_network", ip: "192.168.96.13", :netmask => "255.255.255.0"
 end
 
 config.vm.define "ui" do |ui|
-  dev.vm.hostname = "ui.vagrant.localdomain"
-  dev.vm.network "private_network", ip: "192.168.96.14", :netmask => "255.255.255.0"
+  ui.vm.hostname = "ui.vagrant.localdomain"
+  ui.vm.network "private_network", ip: "192.168.96.14", :netmask => "255.255.255.0"
 end
